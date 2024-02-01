96ca53a64f3c30ed0129aa9e01ce468defdc5083
Sudhanshu <ersshekhar05@gmail.com>
2024-02-01 17:46:17 +0530

    code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { ErrorBoundary } from 'react-error-boundary';
 function App() {
   const apiData = async () => {
     try {
-      const response = await fetch("https://dummyjson.om/products/1");
+      const response = await fetch("https://dummyjson.com/products/1");
       const resData = await response?.json();
       console.log("resData", resData);
     } 
